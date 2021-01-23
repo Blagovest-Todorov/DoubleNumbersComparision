abb0f330f7e5908ea7953efe2b8fbdf50dc371d0
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-23 21:08:36 +0200

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AreEqualTwoDoubleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double numberA = double.Parse(Console.ReadLine());
+            double numberB = double.Parse(Console.ReadLine());
+            double diff = 0.000001;
+            bool areEqual = false;
+
+            if ( (Math.Abs(numberA) - Math.Abs(numberB)) < diff)
+            {
+                areEqual = true;
+            }
+            else
+            {
+                areEqual = false;
+            }
+
+            Console.WriteLine(areEqual);
+            
+        }
+    }
+}
